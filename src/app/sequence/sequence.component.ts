3117c847cc63bac6304ddb4b09fd14987b33bf09
Patrick Suclla <sucllaerazop@gmail.com>
2024-05-10 18:13:43 -0500

    intento para recorrer array y marcar cada cuadro// se logro realizar el generar random

--- src/app/sequence/sequence.component.ts
+++ src/app/sequence/sequence.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LevelComponent } from '../shared/level/level.component';
 
 @Component({
@@ -8,6 +8,96 @@ import { LevelComponent } from '../shared/level/level.component';
   imports: [CommonModule, LevelComponent],
   templateUrl: './sequence.component.html',
 })
-export class SequenceComponent {
+export class SequenceComponent implements OnInit {
   quadrates: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  randomCorrect: number[] = [];
+  level: number = 0;
+  numberBefore: number = 0;
+
+  indentificador: number = 1;
+  identificadorIntervalo: any;
+
+  ngOnInit(): void {
+    // this.runCards(this.randomCorrect);
+  }
+
+  generateLevel() {
+    this.level++;
+    this.randomCorrect.push(this.generateRandom());
+    this.repetir();
+    // this.viewCorrect();
+    console.log(this.randomCorrect);
+  }
+
+  generateRandom(): number {
+    let x = Math.floor(Math.random() * 9 + 1);
+
+    if (this.numberBefore == x) {
+      console.log('entro al if', x);
+      return this.generateRandom();
+    }
+    this.numberBefore = x;
+    console.log('generateRandom p asando if', x);
+    return x;
+  }
+
+  repetir() {
+    console.log('entrando a repetir', this.randomCorrect);
+
+    this.identificadorIntervalo = setInterval(this.abrirTarjeta, 1000, [
+      ...this.randomCorrect,
+    ]);
+  }
+
+  abrirTarjeta(x: number[]) {
+    console.log('entrando a abrirTarjeta x', x);
+    console.log(
+      'entrando a abrirTarjeta this.indentificador',
+      this.indentificador
+    );
+
+    if (x.length > this.indentificador + 1) {
+      clearInterval(this.identificadorIntervalo);
+      console.log(
+        'se ejecuta dentro de la condicional clearInterval',
+        this.indentificador
+      );
+    }
+    console.log('se ejecuta', this.indentificador);
+
+    document
+      .getElementsByClassName('quadrate')
+      [this.randomCorrect[this.indentificador]].classList.add('bg-white');
+    this.indentificador++;
+  }
+
+  nextLevel() {
+    return this.level++;
+  }
+
+  viewCorrect() {
+    for (let i = 0; i < this.randomCorrect.length; i++) {
+      let x = this.viewCard(this.randomCorrect[i], i);
+    }
+
+    // this.randomCorrect.forEach(async (card) => {
+    //   await this.viewCard(card - 1);
+    // });
+  }
+
+  viewCard(card: number, i: number) {
+    setTimeout(this.habilitar, i * 1000, card);
+    setTimeout(this.deshabilitar, i * 2000, card);
+  }
+
+  habilitar(card: number) {
+    document.getElementsByClassName('quadrate')[card].classList.add('bg-white');
+    return;
+  }
+  deshabilitar(card: number) {
+    document
+      .getElementsByClassName('quadrate')
+      [card].classList.remove('bg-white');
+    return;
+  }
 }
