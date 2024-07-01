ffabf5b1a83823e9b8401d1ffe9b0d41048df470
Jorge Casaretto <jorgediogocasaretto280404@gmail.com>
2024-06-30 23:39:01 -0500

    Juegos de William integrados al 99%

--- src/app/components/number-memory/number-memory.component.ts
+++ src/app/components/number-memory/number-memory.component.ts
@@ -1,86 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { StartgameComponent } from '../../shared/startgame/startgame.component';
 
 @Component({
   selector: 'app-number-memory',
   standalone: true,
-  imports: [StartgameComponent],
+  imports: [CommonModule, StartgameComponent],
   templateUrl: './number-memory.component.html',
   styleUrl: './number-memory.component.css',
 })
 export class NumberMemoryComponent {
-  level = 1;
-  numberToRemember: number = 0;
+  pantallaInicial: boolean = true;
+  answerNumber: number = 0;
+  levelScore: number = 1;
   name:string = "Number Memory"
   text:string = "Recuerda todos los numeros que se mostraran";
   estadoComponente:boolean = false;
 
-  ngOnInit() {
+  ngOnInit(): void {
 
   }
 
-
-   startGame(){
-    if (typeof document !== 'undefined') {
-  
-      this.transitionScreens('start-screen', 'number-screen');
-      this.displayNumber();
-    ;
-
-    const submitHandler = () => {
-      const userInput = (
-        document.getElementById('user-input') as HTMLInputElement
-      ).value;
-      document.getElementById('correct-number')!.textContent =
-        this.numberToRemember.toString();
-      document.getElementById('user-answer')!.textContent = userInput;
-      document.getElementById('current-level')!.textContent =
-        this.level.toString();
-
-      this.transitionScreens('input-screen', 'result-screen');
-
-      if (userInput === this.numberToRemember.toString()) {
-        document.getElementById('next-level-button')!.style.display = 'block';
-        this.level++;
-      } else {
-        document.getElementById('next-level-button')!.style.display = 'none';
-        this.level = 1;
-      }
-
-      (document.getElementById('user-input') as HTMLInputElement).value = '';
-    };
-
-    document
-      .getElementById('submit-button')!
-      .addEventListener('click', submitHandler);
-
-    // Escucha el evento keypress en el campo de entrada
-    document
-      .getElementById('user-input')!
-      .addEventListener('keypress', (event) => {
-        if (event.key === 'Enter') {
-          submitHandler();
-        }
-      });
-
-    document
-      .getElementById('try-again-button')!
-      .addEventListener('click', () => {
-        this.level = 1;
-        this.transitionScreens('result-screen', 'start-screen');
-      });
-
-    document
-      .getElementById('next-level-button')!
-      .addEventListener('click', () => {
-        document.getElementById('current-level')!.textContent =
-          this.level.toString();
-        this.transitionScreens('result-screen', 'number-screen');
-        this.displayNumber();
-      });
-  }
+  startGame(){
+    this.levelScore = 1;
+    this.timerLineProcedure();
+    this.nextLevel();
+    this.nextScreenAnswer();
    }
-
 
   receivingState(estate:boolean):void{
     if (estate) {
@@ -88,75 +34,68 @@ export class NumberMemoryComponent {
       this.startGame();
     }
   }
+  
+  timerLineProcedure(): void {
+    setTimeout(() => {
+    let timerLine: HTMLElement | null = document.querySelector(`.timerLine`);
+    timerLine!.style.width = "0px";
+    }, );
+  }
 
-
-
-
-
-
-  displayNumber() {
-    const digits = Math.min(this.level, 9); // Máximo de 9 dígitos
-    const maxNumber = Math.pow(10, digits) - 1;
+  generateRandomNumber(digits: number): number {
     const minNumber = Math.pow(10, digits - 1);
-    this.numberToRemember =
-      Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+    const maxNumber = Math.pow(10, digits) - 1;
 
-    if (typeof document !== 'undefined') {
-      document.getElementById('number-display')!.textContent =
-        this.numberToRemember.toString();
-    }
-    this.initiateTimer(5); // 5 segundos para memorizar el número
+    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
   }
 
-  initiateTimer(duration: number) {
-    if (typeof document !== 'undefined') {
-      console.log('initialTimer');
-      let timerElement = document.getElementById('timer-progress');
-      if (!timerElement) {
-        console.log('initialTimer antes de crearse');
-        this.createProgressElement();
-        timerElement = document.getElementById('timer-progress');
-      }
-      if (timerElement) {
-        timerElement.style.width = '100%';
-      }
-      let timeLeft = duration;
-
-      const countdown = setInterval(() => {
-        timeLeft--;
-        let progress = (timeLeft / duration) * 100;
-        if (timerElement) {
-          timerElement.style.width = progress + '%';
-        }
-
-        if (timeLeft === 0) {
-          clearInterval(countdown);
-          this.transitionScreens('number-screen', 'input-screen');
-        }
-      }, 1000);
-    }
+  nextLevel(): void {
+    setTimeout(() => {
+      this.answerNumber = this.generateRandomNumber(this.levelScore);
+      let numberAnswer: HTMLElement | null = document.querySelector(`.number-main`);
+      numberAnswer!.textContent = this.answerNumber.toString();
+  }, );
   }
 
-  createProgressElement() {
-    console.log('creando el element progress');
+  nextScreenAnswer(): void{
+    setTimeout(() => {
+      let screen1: HTMLElement | null = document.querySelector(`.screen-1`);
+      screen1!.style.display = "none";
+      let screen2: HTMLElement | null = document.querySelector(`.screen-2`);
+      screen2!.style.display = "block";
+      
+    }, 2000);
+  }
 
-    if (typeof document !== 'undefined') {
-      console.log('creando el element progress dentro del if');
-      const progressContainer = document.createElement('div');
-      progressContainer.className = 'progress';
-      const progressBar = document.createElement('div');
-      progressBar.className = 'progress-bar';
-      progressBar.id = 'timer-progress';
-      progressBar.style.width = '100%';
-      progressContainer.appendChild(progressBar);
-      document.getElementById('number-screen')!.appendChild(progressContainer);
+  validateNumber(event: MouseEvent) {
+    let answerInput = document.getElementById('answer-input') as HTMLInputElement;
+    const userAnswer = parseInt(answerInput.value);
+    const correctAnswer = parseInt(this.answerNumber.toString());
+    if(correctAnswer === userAnswer) {
+      this.levelScore++;
+      this.nextScreenLevel();
+      answerInput.value = ''
+      console.log("correcto")
+    }else{
+      this.estadoComponente = false;
+      this.pantallaInicial = false;
+      answerInput.value = ''
+      console.log("incorrecto")
     }
   }
 
-  transitionScreens(hideId: string, showId: string) {
-    if (typeof document !== 'undefined') {
-      document.getElementById(hideId)!.style.display = 'none';
-      document.getElementById(showId)!.style.display = 'block';
-    }
+  nextScreenLevel(): void{
+    let screen1: HTMLElement | null = document.querySelector(`.screen-1`);
+    screen1!.style.display = "block";
+    let screen2: HTMLElement | null = document.querySelector(`.screen-2`);
+    screen2!.style.display = "none";
+
+    this.nextLevel();
+    let timerLine: HTMLElement | null = document.querySelector(`.timerLine`);
+    timerLine!.style.width = "100px";
+    this.timerLineProcedure();
+
+    this.nextScreenAnswer();
   }
+
 }
